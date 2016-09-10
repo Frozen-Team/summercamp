07bbe35b5bac9457156f3a55b1b9552473d4c547
olehgol260 <olehgol260@gmail.com>
2016-09-10 19:41:46 +0300

    Add users controller and a register action. Some other improvements.
    - replace []error with []string for Errors field of form.UserReg, as json cannot marshal error type
    - remove PasswordSalt from User struct
    - rename users.go to user.go in models package
    - add method to ApplicationController to unmarshal json from request body
    - change meta signature in Serve* methods. Now there are 2 fields for error data: has_error, boolean value, indicates whether there is any error or not; the second value is array of errors, if there is only 1 error, it's just an array with 1 entry
    - other minor refactoring and formatting

--- models/forms/helpers.go
+++ models/forms/helpers.go
@@ -1,7 +1,6 @@
 package forms
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/astaxie/beego"
@@ -10,17 +9,17 @@ import (
 
 // validate performs the validation of the form fields based on
 // the `valid` tag for each struct field
-func validate(whatToCheck interface{}) (errs []error, ok bool) {
+func validate(whatToCheck interface{}) (errs []string, ok bool) {
 	valid := validation.Validation{}
 	ok, err := valid.Valid(whatToCheck)
 	if err != nil {
-		errs = append(errs, errors.New("validation-failed"))
+		errs = append(errs, "validation-failed")
 		beego.BeeLogger.Error("validation process error: %v", err)
 		return
 	}
 	if valid.HasErrors() {
 		for _, err := range valid.Errors {
-			e := fmt.Errorf("%v : %v", err.Key, err.Message)
+			e := fmt.Sprintf("%v : %v", err.Key, err.Message)
 			errs = append(errs, e)
 			beego.Warning(e)
 		}
--- models/forms/registration.go
+++ models/forms/registration.go
@@ -1,8 +1,6 @@
 package forms
 
 import (
-	"errors"
-
 	"bitbucket.org/SummerCampDev/summercamp/models"
 	"github.com/astaxie/beego"
 )
@@ -16,7 +14,7 @@ type UserReg struct {
 	PasswordConfirm string            `json:"password_confirm" valid:"Required"`
 	Country         string            `json:"country" valid:"Required"`
 	City            string            `json:"city" valid:"Required"`
-	Errors          []error           `json:"-"`
+	Errors          []string          `json:"-"`
 }
 
 // Register validates the input data and if everything is OK, initialize the models.User struct with
@@ -29,21 +27,28 @@ func (ur *UserReg) Register() (*models.User, bool) {
 	}
 
 	if ur.Password != ur.PasswordConfirm {
-		ur.Errors = append(ur.Errors, errors.New("passwords-mismatch"))
+		ur.Errors = append(ur.Errors, "passwords-mismatch")
 		beego.Warning(ur.Errors[0])
 		return nil, false
 	}
 
-	user := &models.User{Email: ur.Email, Type: ur.Type, FirstName: ur.FirstName, LastName: ur.LastName, Country: ur.Country, City: ur.City}
+	user := &models.User{
+		Email:     ur.Email,
+		Type:      ur.Type,
+		FirstName: ur.FirstName,
+		LastName:  ur.LastName,
+		Country:   ur.Country,
+		City:      ur.City,
+	}
 
 	ok = user.SetPassword(ur.Password)
 	if !ok {
-		ur.Errors = append(ur.Errors, errors.New("user-password-set-failed"))
+		ur.Errors = append(ur.Errors, "user-password-set-failed")
 		return nil, false
 	}
 	ok = user.Save()
 	if !ok {
-		ur.Errors = append(ur.Errors, errors.New("user-save-failed"))
+		ur.Errors = append(ur.Errors, "user-save-failed")
 	}
 	return user, ok
 }
