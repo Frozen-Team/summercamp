07271b9957c30640833a2ab737debb768f772edc
olehgol260 <olehgol260@gmail.com>
2016-09-11 21:49:35 +0300

    Add function to check if the password is strong. Add tests for this function

--- models/forms/registration.go
+++ models/forms/registration.go
@@ -1,6 +1,8 @@
 package forms
 
 import (
+	"unicode"
+
 	"bitbucket.org/SummerCampDev/summercamp/models"
 	"github.com/astaxie/beego"
 )
@@ -23,6 +25,11 @@ func (ur *UserReg) Register() (*models.User, bool) {
 	errs, ok := validate(ur)
 	if !ok {
 		ur.Errors = errs
+		return nil, false
+	}
+
+	if !isStrongPass(ur.Password) {
+		ur.Errors = append(ur.Errors, "password-weak")
 		return nil, false
 	}
 
@@ -51,4 +58,33 @@ func (ur *UserReg) Register() (*models.User, bool) {
 		ur.Errors = append(ur.Errors, "user-save-failed")
 	}
 	return user, ok
+}
+
+// isStringPass checks the password if it is strong enough. The minimum length is 5 symbols.
+// The password must contain at least 2 of the following: upper letter, lower letter or number OR it must contain
+// some special symbol which is not any of the described before.
+func isStrongPass(p string) bool {
+	len := len(p)
+	var num, upper, lower byte
+	if len < 5 {
+		return false
+	}
+	for _, c := range p {
+		if unicode.IsNumber(c) {
+			num = 1
+			continue
+		} else if unicode.IsUpper(c) {
+			upper = 1
+			continue
+		}
+		if unicode.IsLower(c) {
+			lower = 1
+			continue
+		}
+		if !(unicode.IsLetter(c) || unicode.IsNumber(c)) {
+			num = 2
+			break
+		}
+	}
+	return (num + upper + lower) >= 2
 }
