b1fe50867109655cdf92923c6da2e554b3bbed27
olehgol260 <olehgol260@gmail.com>
2016-09-15 23:43:14 +0300

    Add some useful teamMember api methods

--- models/team_member.go
+++ models/team_member.go
@@ -32,3 +32,35 @@ func (tm *TeamMember) Save() bool {
 
 	return utils.ProcessError(err, action+" team member")
 }
+
+type teamsMembersAPI struct{}
+
+var TeamMembers *teamsMembersAPI
+
+// FetchByID fetch a team from the teams table by id
+func (tm *teamsMembersAPI) FetchByID(id int) (*TeamMember, bool) {
+	teamMember := TeamMember{ID: id}
+	err := DB.Read(&teamMember)
+	return &teamMember, utils.ProcessError(err, "fetch the team member by id")
+}
+
+// FetchAll fetches all teams from the users table
+func (tm *teamsMembersAPI) FetchAll() ([]TeamMember, bool) {
+	var teamMembers []TeamMember
+	_, err := DB.QueryTable(TeamMemberObj).All(&teamMembers)
+	return teamMembers, utils.ProcessError(err, "fetch all team members")
+}
+
+// FetchTeamsByMember fetch all teams in which the user with the teamMemberID is a member
+func (tm *teamsMembersAPI) FetchTeamsByMember(teamMemberID int) ([]int, bool) {
+	var teamIDs []int
+	_, err := DB.Raw("SELECT team_id FROM team_members WHERE user_id=$1;", teamMemberID).QueryRows(&teamIDs)
+	return teamIDs, utils.ProcessError(err, "fetch teams by member")
+}
+
+// FetchMembersByTeam fetch all members of the team with the given teamID
+func (tm *teamsMembersAPI) FetchMembersByTeam(teamID int) ([]int, bool) {
+	var memberIDs []int
+	_, err := DB.Raw("SELECT user_id FROM team_members WHERE team_id=$1", teamID).QueryRows(&memberIDs)
+	return memberIDs, utils.ProcessError(err, "fetch members by team")
+}
