badd85fc9d0642324b796126aa22115ca284abce
olehgol260 <olehgol260@gmail.com>
2016-09-22 00:12:03 +0300

    Add project skill model definition

--- models/model_objects.go
+++ models/model_objects.go
@@ -2,7 +2,8 @@ package models
 
 // model objects
 var (
-	UserModel       = new(User)
-	TeamModel       = new(Team)
-	TeamMemberModel = new(TeamMember)
+	UserModel         = new(User)
+	TeamModel         = new(Team)
+	TeamMemberModel   = new(TeamMember)
+	ProjectSkillModel = new(ProjectSkill)
 )
