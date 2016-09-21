38790ab698ffa23ecb6b2d7aa92c540dda713dc3
olehgol260 <olehgol260@gmail.com>
2016-09-22 00:45:36 +0300

    Add ProjectSkillsAPI and one helper method

--- models/project_skill.go
+++ models/project_skill.go
@@ -1,6 +1,9 @@
 package models
 
-import "bitbucket.org/SummerCampDev/summercamp/models/utils"
+import (
+	"bitbucket.org/SummerCampDev/summercamp/models/utils"
+	"github.com/astaxie/beego"
+)
 
 type ProjectSkill struct {
 	ID        int `json:"id" orm:"column(id)"`
@@ -39,4 +42,29 @@ func (ps *ProjectSkill) Delete() bool {
 	_, err := DB.Delete(ps)
 
 	return utils.ProcessError(err, " delete a project`s skill")
+}
+
+type ProjectSkillsAPI struct{}
+
+var ProjectSkills *ProjectSkillsAPI
+
+// SaveSkillsForProject create a new ProjectSkill record for each skillID from skillIDs and projectID pair.
+// If each record is successfully saved to the db, the func return false
+func (ps *ProjectSkillsAPI) SaveSkillsForProject(projectID int, skillIDs ...int) bool {
+	if len(skillIDs) == 0 {
+		beego.BeeLogger.Warning("Empty skills list is passed to SaveSkillsForProject")
+		return false
+	}
+
+	for _, skillID := range skillIDs {
+		projectSkill := ProjectSkill{
+			ProjectID: projectID,
+			SkillID:   skillID,
+		}
+		if ok := projectSkill.Save(); !ok {
+			return false
+		}
+	}
+
+	return true
 }
