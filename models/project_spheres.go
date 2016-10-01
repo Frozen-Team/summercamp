fc2a701b742b63ebc198ff2eee22031c15a2048c
olehgol260 <olehgol260@gmail.com>
2016-10-01 13:35:03 +0300

    Add api for project_sphere model

--- models/project_spheres.go
+++ models/project_spheres.go
@@ -1,6 +1,9 @@
 package models
 
-import "bitbucket.org/SummerCampDev/summercamp/models/utils"
+import (
+	"bitbucket.org/SummerCampDev/summercamp/models/utils"
+	"github.com/astaxie/beego"
+)
 
 type ProjectSphere struct {
 	ID        int `json:"id" orm:"column(id)"`
@@ -39,4 +42,61 @@ func (ps *ProjectSphere) Delete() bool {
 	_, err := DB.Delete(ps)
 
 	return utils.ProcessError(err, " delete a project`s sphere")
+}
+
+type ProjectSpheresAPI struct{}
+
+var ProjectSpheres *ProjectSpheresAPI
+
+// SaveSpheresForProject create a new ProjectSphere record for each sphereID from sphereIDs and projectID pair.
+// If each record is successfully saved to the db, the func return false
+func (ps *ProjectSpheresAPI) SaveSpheresForProject(projectID int, sphereIDs ...int) bool {
+	if len(sphereIDs) == 0 {
+		beego.BeeLogger.Warning("Empty spheres list is passed to SaveSpheresForProject")
+		return false
+	}
+
+	var failedSpheres []int
+	for _, sphereID := range sphereIDs {
+		projectSphere := ProjectSphere{
+			ProjectID: projectID,
+			SphereID:  sphereID,
+		}
+		if ok := projectSphere.Save(); !ok {
+			failedSpheres = append(failedSpheres, sphereID)
+		}
+	}
+	ok := len(failedSpheres) == 0
+	if !ok {
+		beego.BeeLogger.Warning("Failed to save project spheres for spheres with ids: '%v'", failedSpheres)
+	}
+	return ok
+}
+
+// FetchSpheresByProject fetch all spheres for a given project
+func (ps *ProjectSpheresAPI) FetchSpheresByProject(projectID int) ([]Sphere, bool) {
+	var spheres []Sphere
+	_, err := DB.Raw(`
+	SELECT spheres.id,
+	       spheres.name
+	FROM project_spheres ps
+	LEFT OUTER JOIN spheres ON spheres.id=ps.sphere_id
+	WHERE ps.project_id=$1;`, projectID).QueryRows(&spheres)
+	return spheres, utils.ProcessError(err, " fetch spheres by a project id")
+}
+
+// FetchProjectsBySpheres fetch all projects for a given sphere id
+func (ps *ProjectSpheresAPI) FetchProjectsBySphere(sphereID int) ([]Project, bool) {
+	var projects []Project
+	_, err := DB.Raw(`
+	SELECT projects.id,
+	       projects.description,
+	       projects.budget,
+	       projects.client_id,
+	       projects.create_time,
+	       projects.update_time
+	FROM project_spheres ps
+	LEFT OUTER JOIN projects ON projects.id=ps.project_id
+	WHERE ps.sphere_id=$1;`, sphereID).QueryRows(&projects)
+	return projects, utils.ProcessError(err, " fetch projects by a sphere id")
 }
