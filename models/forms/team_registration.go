77b95fbc681b82ecb7bad7c0f53276fa9eec6b9c
OTER <maxymhammer@yandex.ru>
2016-10-02 01:48:53 +0300

    Add missing files

---
+++ models/forms/team_registration.go
@@ -0,0 +1,33 @@
+package forms
+
+import (
+	"bitbucket.org/SummerCampDev/summercamp/models"
+)
+
+type TeamRegistration struct {
+	FormModel
+	Name        string	`json:"email" valid:"Required"`
+	Description string	`json:"description" valid:"Required"`
+}
+
+// Register validates the input data and if everything is OK, initialize the models.Team struct with
+// the data from Registration struct and save the record to the db.
+func (tr *TeamRegistration) Register(current *models.User) (*models.Team, bool) {
+	if ok := tr.validate(tr); !ok {
+		return nil, false
+	}
+	team := &models.Team{
+		Name:     	tr.Name,
+		Description:	tr.Description,
+	}
+	ok := team.Save()
+	if !ok {
+		tr.addError("team-save-failed")
+		return nil, false
+	}
+	ok = team.AddMember(current, models.LevelAdmin)
+	if !ok {
+		return nil, team.Delete()
+	}
+	return team, true
+}
