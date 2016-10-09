6fc9646b7aa3696520f0c60fc06957fc7bfbe4b7
olehgol260 <olehgol260@gmail.com>
2016-10-09 20:37:46 +0300

    Add UserSkill model definition

--- models/model_objects.go
+++ models/model_objects.go
@@ -12,4 +12,5 @@ var (
 	ProjectSkillModel  = new(ProjectSkill)
 	ProjectSphereModel = new(ProjectSphere)
 	UserSphereModel    = new(UserSphere)
+	UserSkillModel     = new(UserSkill)
 )
---
+++ models/user_skill.go
@@ -0,0 +1,42 @@
+package models
+
+import "bitbucket.org/SummerCampDev/summercamp/models/utils"
+
+type UserSkill struct {
+	ID      int `json:"id" orm:"column(id)"`
+	UserID  int `json:"user_id" orm:"column(user_id)"`
+	SkillID int `json:"skill_id" orm:"column(skill_id)"`
+}
+
+// TableName specify the table name for UserSkill model. This name is used in the orm RegisterModel
+func (ps *UserSkill) TableName() string {
+	return "user_skills"
+}
+
+// Save insert a new record to the db if ID field is of default value. Otherwise an existing
+// record is updated.
+func (ps *UserSkill) Save() bool {
+	var err error
+	var action string
+
+	if ps.ID == 0 {
+		_, err = DB.Insert(ps)
+		action = "create"
+	} else {
+		_, err = DB.Update(ps)
+		action = "update"
+	}
+
+	return utils.ProcessError(err, action+" a user`s skill")
+}
+
+// Delete deletes a record from the db. If the record is successfully deleted, the return value
+// is true, false - otherwise.
+func (ps *UserSkill) Delete() bool {
+	if ps.ID == 0 {
+		return false
+	}
+	_, err := DB.Delete(ps)
+
+	return utils.ProcessError(err, " delete a user`s skill")
+}
