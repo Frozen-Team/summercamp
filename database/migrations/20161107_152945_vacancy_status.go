dce6d49240e48a363f4ee3a355c4d81c29bd8fbb
olehgol260 <olehgol260@gmail.com>
2016-11-07 15:35:42 +0200

    Add migration to add status column for vacancy table

---
+++ database/migrations/20161107_152945_vacancy_status.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type VacancyStatus_20161107_152945 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &VacancyStatus_20161107_152945{}
+	m.Created = "20161107_152945"
+	migration.Register("VacancyStatus_20161107_152945", m)
+}
+
+// Run the migrations
+func (m *VacancyStatus_20161107_152945) Up() {
+	m.SQL(`CREATE TYPE vacancy_status AS ENUM ('archived', 'active');
+	ALTER TABLE public.vacancy ADD status VACANCY_STATUS DEFAULT 'active' NOT NULL;`)
+}
+
+// Reverse the migrations
+func (m *VacancyStatus_20161107_152945) Down() {
+	m.SQL(`DROP TYPE IF EXISTS vacancy_status;
+	ALTER TABLE IF EXISTS vacancy DROP COLUMN IF EXISTS status;`)
+}
