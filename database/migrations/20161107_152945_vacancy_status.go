ef9ec37b81aeddd3b8f7120458dabdda7cb3ed17
olehgol260 <olehgol260@gmail.com>
2016-11-08 11:13:39 +0200

    Fix the migration for vacancy status (invalid table name was used)

--- database/migrations/20161107_152945_vacancy_status.go
+++ database/migrations/20161107_152945_vacancy_status.go
@@ -19,11 +19,11 @@ func init() {
 // Run the migrations
 func (m *VacancyStatus_20161107_152945) Up() {
 	m.SQL(`CREATE TYPE vacancy_status AS ENUM ('archived', 'active');
-	ALTER TABLE public.vacancy ADD status VACANCY_STATUS DEFAULT 'active' NOT NULL;`)
+	ALTER TABLE public.vacancies ADD status VACANCY_STATUS DEFAULT 'active' NOT NULL;`)
 }
 
 // Reverse the migrations
 func (m *VacancyStatus_20161107_152945) Down() {
 	m.SQL(`DROP TYPE IF EXISTS vacancy_status;
-	ALTER TABLE IF EXISTS vacancy DROP COLUMN IF EXISTS status;`)
+	ALTER TABLE IF EXISTS vacancies DROP COLUMN IF EXISTS status;`)
 }
